ed46737a7a4fa14359fa14a5b3719c70051503e8
Konstantin Leontyev <K.A.Leontyev@gmail.com>
2025-05-17 12:05:10 +0400

    fix(eslint): delete rule dublicate

--- eslint.config.js
+++ eslint.config.js
@@ -144,7 +144,6 @@ export default tseslint.config([
       ],
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/explicit-module-boundary-types': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-inferrable-types': [
         'error',
         {
